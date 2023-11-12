0377817353cac7b9c870ca629c8e28c8ff44477a
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2023-11-12 21:58:00 +0800

    [mod] the swerveModule

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,12 +11,9 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetainConstants;
 import frc.robot.Constants.ModuleConstants;
@@ -31,29 +28,12 @@ public class SwerveModule extends SubsystemBase {
 
   private final RelativeEncoder driveEncoder;
 
-  private final PIDController drivePIDController = new PIDController(ModuleConstants.kPModuleDriveController, 0, 0);
-
-  private final double kModuleMaxAngularVelocity;
-
-  //feedforward sensor-now no use
-  private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(1, 1);;
-  private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(1, 1);
-
-  // Using a TrapezoidProfile PIDController to allow for smooth turning
-  // the constants need to test
-  private final ProfiledPIDController turningPIDController = new ProfiledPIDController(
-      ModuleConstants.kPModuleTurningController,
-      0,
-      0,
-      new TrapezoidProfile.Constraints(
-          ModuleConstants.kMaxModuleAngularSpeedRadiansPerSecond,
-          ModuleConstants.kMaxModuleAngularAccelerationRadiansPerSecondSquared));
+  private final PIDController rotController;
 
   public SwerveModule(int driveMotorChannel,
       int turningMotorChannel,
-      int turningEncoderChannelA) {
-    // set the max speed
-    kModuleMaxAngularVelocity = DrivetainConstants.kMaxAngularSpeed;
+      int turningEncoderChannelA, boolean driveInverted) {
+
     driveMotor = new CANSparkMax(driveMotorChannel, MotorType.kBrushless);
     turningMotor = new CANSparkMax(turningMotorChannel, MotorType.kBrushless);
 
@@ -62,7 +42,29 @@ public class SwerveModule extends SubsystemBase {
 
     driveEncoder = driveMotor.getEncoder();
 
-    turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+    driveMotor.setInverted(driveInverted);
+    turningMotor.setInverted(false);
+
+    rotController = new PIDController(driveMotorChannel, 0, 0);
+    rotController.enableContinuousInput(-180.0, 180.0);
+
+    resetAllEncoder();
+    clearSticklyFault();
+    stopModule();
+  }
+
+  public void resetAllEncoder() {
+    driveEncoder.setPosition(0);
+  }
+
+  public void clearSticklyFault() {
+    driveMotor.clearFaults();
+    turningMotor.clearFaults();
+  }
+
+  public void stopModule() {
+    driveMotor.set(0);
+    turningMotor.set(0);
   }
 
   // to get the single swerveModule speed and the turning rate
@@ -76,60 +78,49 @@ public class SwerveModule extends SubsystemBase {
     return driveEncoder.getPosition() / 6.75 * 2 * Math.PI * ModuleConstants.kWheelRadius;
   }
 
+  // calculate the rate of the drive
+  public double getDriveRate() {
+    return driveEncoder.getVelocity() / 60.0 / 6.75 * 2 * Math.PI * ModuleConstants.kWheelRadius;
+  }
+
+  // to get rotation of turning motor
+  public double getRotation() {
+    return turningEncoder.getAbsolutePosition();
+  }
+
   // to the get the postion by wpi function
   public SwerveModulePosition getPosition() {
     return new SwerveModulePosition(
         getDriveDistance(), new Rotation2d(Math.toRadians(turningEncoder.getAbsolutePosition())));
   }
 
-  public void setDriveMotorReverse(){
-    driveMotor.setInverted(true);
+  public double[] optimizeOutputVoltage(SwerveModuleState goalState, double currentTurningDegree) {
+    goalState = SwerveModuleState.optimize(goalState, Rotation2d.fromDegrees(currentTurningDegree));
+    double driveMotorVoltage = ModuleConstants.kDesireSpeedtoMotorVoltage * goalState.speedMetersPerSecond;
+    double turningMotorVoltage = rotController.calculate(currentTurningDegree, goalState.angle.getDegrees());
+    double[] moduleState = { driveMotorVoltage, turningMotorVoltage };
+    return moduleState;
   }
 
-  public void setTurningMotorReverse(){
-    turningMotor.setInverted(true);
-  }
-
-  public void setDesiredState(SwerveModuleState desiredState, double minus) {
-    // Optimize the reference state to avoid spinning further than 90 degrees
-    SwerveModuleState state = SwerveModuleState.optimize(desiredState, new Rotation2d(Math.toRadians(turningEncoder.getAbsolutePosition())));
-
-    // Calculate the drive output from the drive PID controller.
-    final double driveOutput = drivePIDController.calculate(getDriveRate(),
-        state.speedMetersPerSecond);
-
-    final double kdriveFeedforward = driveFeedforward.calculate(state.speedMetersPerSecond);
-
-    // Calculate the turning motor output from the turning PID controller.
-    final double turnOutput = -turningPIDController.calculate(turningEncoder.getAbsolutePosition(),
-        state.angle.getDegrees());
-
-    final double kturnFeedforward = turnFeedforward.calculate(turningPIDController.getSetpoint().velocity);
-
-    driveMotor.setVoltage(driveOutput);
-    turningMotor.setVoltage(minus*turnOutput);
-  }
-
-  // calculate the rate of the drive
-  public double getDriveRate(){
-    return driveEncoder.getVelocity() / 60.0 / 6.75 * 2 * Math.PI * ModuleConstants.kWheelRadius;
-  }
-
-  // for one module test
-  public double getRotation() {
-    return turningEncoder.getAbsolutePosition();
+  public void setDesiredState(SwerveModuleState desiredState) {
+    if (Math.abs(desiredState.speedMetersPerSecond) < DrivetainConstants.kMinSpeed) {
+      stopModule();
+    } else {
+      var moduleState = optimizeOutputVoltage(desiredState, getRotation());
+      driveMotor.setVoltage(moduleState[0]);
+      turningMotor.setVoltage(moduleState[1]);
+    }
   }
 
   // for one module test
   public void setMotorPower(double driveSpd, double rotSpd) {
     driveMotor.set(0.6 * driveSpd);
     turningMotor.set(rotSpd);
-  } 
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  
 }
